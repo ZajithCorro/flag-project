6442f797caba8cb61c504674641b4f515d01e419
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-08 20:50:24 -0500

    Añadiendo más información para mostrar y función para redireccionar al seleccionar un país

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,49 +1,75 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CountryStyled = styled.article`
-	cursor: pointer;
-	border-radius: 5px;
-	overflow: hidden;
-	box-shadow: 0 0 7px 2px rgba(0, 0, 0, 0.03);
-	background-color: var(--secondary);
-	transition: all 0.3s ease;
+  cursor: pointer;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 7px 2px rgba(0, 0, 0, 0.03);
+  background-color: var(--secondary);
+  transition: all 0.3s ease;
 
-	&:hover {
-		transform: translatey(-0.2rem);
-	}
+  &:hover {
+    transform: translatey(-0.2rem);
+  }
 
-	img {
-		width: 100%;
-		height: 160px;
-		object-fit: cover;
-		vertical-align: top;
-	}
+  img {
+    width: 100%;
+    height: 160px;
+    object-fit: cover;
+    vertical-align: top;
+  }
 `;
 
 const Details = styled.div`
-	padding: 1.5em;
+  padding: 1.5em;
 
-	h2 {
-		margin: 0;
-		margin-bottom: 1rem;
-		font-size: 18px;
-		font-weight: 700;
-	}
+  h2 {
+    margin: 0;
+    margin-bottom: 1rem;
+    font-size: 18px;
+    font-weight: 700;
+  }
 
-	p {
-		font-size: 0.9em;
-		margin-bottom: 0.5rem;
-	}
+  p {
+    font-size: 0.9em;
+    margin-bottom: 0.5rem;
+  }
 `;
 
-export default function Country({ name, flag }) {
-	return (
-		<CountryStyled>
-			<img loading='lazy' src={flag} alt='' />
-			<Details>
-				<h2>{name}</h2>
-			</Details>
-		</CountryStyled>
-	);
+export default function Country({
+  name,
+  flag,
+  capital,
+  population,
+  region,
+  alpha2Code,
+}) {
+  const history = useHistory();
+
+  function goToCountry() {
+    history.push(`/country/${alpha2Code}`);
+  }
+
+  return (
+    <CountryStyled onClick={goToCountry}>
+      <img loading='lazy' src={flag} alt='Flag country' />
+      <Details>
+        <h2>{name}</h2>
+        <p>
+          <strong>Population: </strong>
+          {new Intl.NumberFormat().format(population)}
+        </p>
+        <p>
+          <strong>Capital: </strong>
+          {capital}
+        </p>
+        <p>
+          <strong>Region: </strong>
+          {region}
+        </p>
+      </Details>
+    </CountryStyled>
+  );
 }
