39cc460462c5dc94563b33f86c489d025fedea98
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-09 14:32:01 -0500

    Página Country
    Toma el alpha2Code que viene por la ruta y busca el país. Si el país lo tiene en el estado global lo renderiza, si no lo tiene lo solicita la API. Agregando estilos

--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { CountryContext } from '../Context/CountryContext';
 
-export default function Country() {
-	return <div>Country</div>;
+import CountryDescription from '../Components/CountryDescription';
+import Wrapper from '../Components/Wrapper';
+
+const CountryPage = styled.section``;
+
+const Back = styled.button`
+	background-color: var(--secondary);
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+	padding: 0.7em 2.2em;
+	border-radius: 5px;
+	border: none;
+	cursor: pointer;
+	color: var(--primary);
+
+	i {
+		margin-right: 5px;
+	}
+`;
+
+export default function Country({ match, history }) {
+	const { state } = useContext(CountryContext);
+
+	const contextCountry = state.countryList.find(
+		(item) => item.alpha2Code === match.params.id
+	);
+
+	const [country, setCountry] = useState(contextCountry);
+
+	useEffect(() => {
+		async function fetchCountry() {
+			const response = await fetch(
+				`https://restcountries.eu/rest/v2/alpha/${match.params.id}`
+			);
+			const data = await response.json();
+			setCountry(data);
+		}
+
+		if (!contextCountry) fetchCountry();
+	}, [match.params.id, contextCountry]);
+
+	return (
+		<CountryPage>
+			<Wrapper>
+				<Back onClick={() => history.goBack()}>
+					<i className='fas fa-long-arrow-alt-left'></i> Back
+				</Back>
+				<CountryDescription {...country} />
+			</Wrapper>
+		</CountryPage>
+	);
 }
