10be60145a6ffec7d366be2be42ca086992fa6d7
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-10 13:28:25 -0500

    Updates

---
+++ precache-manifest.dd67f803433a527725687cc9540c90b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13aeb50812d632633b48422bace3c4d5",
+    "url": "/flag-project/index.html"
+  },
+  {
+    "revision": "e5a34223fc0c61341ead",
+    "url": "/flag-project/static/css/main.268cc84d.chunk.css"
+  },
+  {
+    "revision": "06b7de93782e4bd0b53e",
+    "url": "/flag-project/static/js/2.552f2f71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flag-project/static/js/2.552f2f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a34223fc0c61341ead",
+    "url": "/flag-project/static/js/main.3b98083d.chunk.js"
+  },
+  {
+    "revision": "24304f265790aeaf8ec2",
+    "url": "/flag-project/static/js/runtime-main.6f8646ab.js"
+  }
+]);
