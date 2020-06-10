f59b8e58b3d4dd80a18bba6320020d10d5f4cb77
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-10 12:57:53 -0500

    Quitando padding generaba inconsistencia

--- src/Components/CountryList.jsx
+++ src/Components/CountryList.jsx
@@ -6,61 +6,60 @@ import Wrapper from './Wrapper';
 import Country from './Country';
 
 const CountryListStyled = styled.div`
-  display: grid;
-  grid-gap: 2.3em 4em;
-  grid-auto-flow: columns;
-  grid-template-columns: repeat(auto-fill, 270px);
-  justify-content: center;
-  padding: 3em 0;
+	display: grid;
+	grid-gap: 2.3em 4em;
+	grid-auto-flow: columns;
+	grid-template-columns: repeat(auto-fill, 270px);
+	justify-content: center;
 `;
 
 export default function CountryList() {
-  const { state, setCountryList } = useContext(CountryContext);
+	const { state, setCountryList } = useContext(CountryContext);
 
-  const { countryList } = state;
+	const { countryList } = state;
 
-  useEffect(() => {
-    const fetchCountries = () => {
-      fetch('https://restcountries.eu/rest/v2/all')
-        .then((response) => response.json())
-        .then((data) => {
-          setCountryList(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
+	useEffect(() => {
+		const fetchCountries = () => {
+			fetch('https://restcountries.eu/rest/v2/all')
+				.then((response) => response.json())
+				.then((data) => {
+					setCountryList(data);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		};
 
-    fetchCountries();
-  }, []);
+		fetchCountries();
+	}, []);
 
-  return (
-    <Wrapper>
-      <CountryListStyled>
-        {countryList.map(
-          ({
-            name,
-            flag,
-            capital,
-            population,
-            numericCode,
-            alpha3Code,
-            region,
-          }) => {
-            return (
-              <Country
-                key={numericCode}
-                name={name}
-                flag={flag}
-                capital={capital}
-                population={population}
-                region={region}
-                alpha3Code={alpha3Code}
-              />
-            );
-          }
-        )}
-      </CountryListStyled>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<CountryListStyled>
+				{countryList.map(
+					({
+						name,
+						flag,
+						capital,
+						population,
+						numericCode,
+						alpha3Code,
+						region,
+					}) => {
+						return (
+							<Country
+								key={numericCode}
+								name={name}
+								flag={flag}
+								capital={capital}
+								population={population}
+								region={region}
+								alpha3Code={alpha3Code}
+							/>
+						);
+					}
+				)}
+			</CountryListStyled>
+		</Wrapper>
+	);
 }
