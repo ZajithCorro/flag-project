35bb3134ebd8f36d2eec4eb9efecee4ff9b301bd
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-10 12:59:58 -0500

    Agregando dark mode en la aplicación. La aplicación detecta cuando el dispositivo esta en modo noche

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CountryProvider } from './Context/CountryContext';
 import Root from './Root';
-
+import Header from './Components/Header';
 import './App.css';
 
 export default function App() {
+	const [darkMode, setDarkMode] = useState(false);
+	const mainClass = darkMode ? 'is-dark-mode' : 'is-light-mode';
+
+	useEffect(() => {
+		const mq = window.matchMedia('(prefers-color-scheme: dark)');
+		mq.addListener(() => setDarkMode(mq.matches));
+		setDarkMode(mq.matches);
+		return () => {
+			mq.removeListener();
+		};
+	}, []);
+
 	return (
-		<main>
+		<main className={mainClass}>
 			<CountryProvider>
 				<Router>
+					<Header setDarkMode={setDarkMode} darkMode={darkMode} />
 					<Root />
 				</Router>
 			</CountryProvider>
