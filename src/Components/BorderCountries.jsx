94018ef7c3018e73fd367755ecc4cd9819e49353
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-10 12:59:03 -0500

    Agregando un hover en los países

--- src/Components/BorderCountries.jsx
+++ src/Components/BorderCountries.jsx
@@ -3,52 +3,56 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
 const BorderCountriesStyled = styled.div`
-  text-align: center;
-  margin-top: 1.5em;
+	text-align: center;
+	margin-top: 1.5em;
 
-  .list-countries {
-    margin-top: 1em;
-  }
-  .country {
-    cursor: pointer;
-    padding: 0.5em 2em;
-    margin: 0 15px 15px 0;
-    border-radius: 5px;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-    background: var(--secondary);
-    display: inline-flex;
-  }
+	.list-countries {
+		margin-top: 1em;
+	}
+	.country {
+		cursor: pointer;
+		padding: 0.5em 2em;
+		margin: 0 15px 15px 0;
+		border-radius: 5px;
+		box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+		background: var(--secondary);
+		display: inline-flex;
+		transition: all 0.3s ease;
 
-  @media screen and (min-width: 900px) {
-    text-align: left;
-  }
+		&:hover {
+			transform: translatey(-0.2rem);
+		}
+	}
+
+	@media screen and (min-width: 900px) {
+		text-align: left;
+	}
 `;
 
 export default function BorderCountries({ countries }) {
-  const history = useHistory();
+	const history = useHistory();
 
-  function goTo(countryCode) {
-    history.push(`/country/${countryCode}`);
-  }
+	function goTo(countryCode) {
+		history.push(`/country/${countryCode}`);
+	}
 
-  return (
-    <BorderCountriesStyled>
-      <section>
-        <div>
-          <strong>Border Countries:</strong>
-        </div>
-        <div className='list-countries'>
-          {countries.map((country) => (
-            <span
-              className='country'
-              key={country}
-              onClick={() => goTo(country)}
-            >
-              {country}
-            </span>
-          ))}
-        </div>
-      </section>
-    </BorderCountriesStyled>
-  );
+	return (
+		<BorderCountriesStyled>
+			<section>
+				<div>
+					<strong>Border Countries:</strong>
+				</div>
+				<div className='list-countries'>
+					{countries.map((country) => (
+						<span
+							className='country'
+							key={country}
+							onClick={() => goTo(country)}>
+							{country}
+						</span>
+					))}
+				</div>
+			</section>
+		</BorderCountriesStyled>
+	);
 }
