8f01910a2ef6d869149c32241467405fe5fd7dbf
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-09 23:00:33 -0500

    Modificando los estilos para no tener múltiples elementos

--- src/Components/CountryDescription.jsx
+++ src/Components/CountryDescription.jsx
@@ -1,149 +1,110 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import CountryBorder from './CountryBorder';
+import BorderCountries from './BorderCountries';
 
-const Container = styled.div`
-	margin-top: 3em;
-	padding-bottom: 3em;
+const CountryDescriptionStyled = styled.div`
+  margin-top: 3em;
+  padding-bottom: 3em;
+  text-align: center;
 
-	img {
-		width: 100%;
-		margin-bottom: 2em;
-	}
+  img {
+    width: 100%;
+    margin-bottom: 2em;
+  }
 
-	@media screen and (min-width: 900px) {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		grid-column-gap: 120px;
-		margin-top: 5em;
-	}
-`;
+  .details {
+    h2 {
+      margin-bottom: 2em;
+    }
 
-const Details = styled.div`
-	text-align: center;
+    p {
+      margin-bottom: 1em;
+    }
+  }
 
-	h2 {
-		margin-bottom: 2em;
-	}
+  .grid {
+    display: grid;
+    gap: 10px 0;
+  }
 
-	p {
-		margin-bottom: 1em;
-	}
+  @media screen and (min-width: 900px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 120px;
+    margin-top: 5em;
+    text-align: left;
 
-	@media screen and (min-width: 900px) {
-		text-align: left;
-	}
-`;
-
-const Language = styled.span`
-	margin-right: 5px;
-
-	&::after {
-		content: ',';
-	}
-
-	&:last-child {
-		&::after {
-			content: '';
-		}
-	}
-`;
-
-const BorderCountries = styled.div`
-	text-align: center;
-	margin-top: 1.5em;
-
-	.list-countries {
-		margin-top: 1em;
-	}
-
-	@media screen and (min-width: 900px) {
-		text-align: left;
-	}
+    .grid {
+      grid-template-columns: 50% 50%;
+      gap: 0 10px;
+    }
+  }
 `;
 
 export default function CountryDescription({
-	flag,
-	name,
-	nativeName,
-	population,
-	area,
-	region,
-	subregion,
-	capital,
-	currencies = [],
-	languages = [],
-	borders = [],
-	alpha2Code,
+  flag,
+  name,
+  nativeName,
+  population,
+  area,
+  region,
+  subregion,
+  capital,
+  currencies = [],
+  borders = [],
+  topLevelDomain,
 }) {
-	return (
-		<Container>
-			<img src={flag} alt='Bandera' />
-			<Details>
-				<h2>{name}</h2>
-				<div>
-					<section>
-						<p>
-							<strong>Native name: </strong>
-							{nativeName}
-						</p>
-						<p>
-							<strong>Capital: </strong>
-							{capital}
-						</p>
-						<p>
-							<strong>Area: </strong>
-							{new Intl.NumberFormat().format(area)} Km<sup>2</sup>
-						</p>
-						<p>
-							<strong>Region: </strong>
-							{region}
-						</p>
-						<p>
-							<strong>Sub Region: </strong>
-							{subregion}
-						</p>
-						<p>
-							<strong>Population: </strong>
-							{new Intl.NumberFormat().format(population)}
-						</p>
-					</section>
-					<section>
-						<p>
-							<strong>Currencies: </strong>
-							{currencies.map((item) => (
-								<span>
-									{item.name} - {item.symbol}
-								</span>
-							))}
-						</p>
-						<p>
-							<strong>Languages: </strong>
-							{languages.map((language) => (
-								<Language>{language.name}</Language>
-							))}
-						</p>
-					</section>
-				</div>
+  return (
+    <CountryDescriptionStyled>
+      <img src={flag} alt='Bandera' />
+      <div className='details'>
+        <h2>{name}</h2>
+        <div className='grid'>
+          <section>
+            <p>
+              <strong>Native name: </strong>
+              {nativeName}
+            </p>
+            <p>
+              <strong>Capital: </strong>
+              {capital}
+            </p>
+            <p>
+              <strong>Area: </strong>
+              {new Intl.NumberFormat().format(area)} Km<sup>2</sup>
+            </p>
+            <p>
+              <strong>Region: </strong>
+              {region}
+            </p>
+            <p>
+              <strong>Sub Region: </strong>
+              {subregion}
+            </p>
+          </section>
+          <section>
+            <p>
+              <strong>Population: </strong>
+              {new Intl.NumberFormat().format(population)}
+            </p>
+            <p>
+              <strong>Domain: </strong>
+              {topLevelDomain}
+            </p>
+            <p>
+              <strong>Currencies: </strong>
+              {currencies.map((item) => (
+                <span key={item.name}>
+                  {item.name} - {item.symbol}
+                </span>
+              ))}
+            </p>
+          </section>
+        </div>
 
-				{borders.length > 0 ? (
-					<BorderCountries>
-						<section>
-							<div>
-								<strong>Border Countries:</strong>
-							</div>
-							<div className='list-countries'>
-								{borders.map((country) => (
-									<CountryBorder name={country} />
-								))}
-							</div>
-						</section>
-					</BorderCountries>
-				) : (
-					''
-				)}
-			</Details>
-		</Container>
-	);
+        {borders.length > 0 ? <BorderCountries countries={borders} /> : ''}
+      </div>
+    </CountryDescriptionStyled>
+  );
 }
