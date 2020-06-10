d512879637ef0edd688d37cd749e96b2fa19e9ac
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-10 12:56:08 -0500

    Agregando header a la aplicación

---
+++ src/Components/Header.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Wrapper from './Wrapper';
+
+const HeaderStyled = styled.div`
+	background: var(--secondary);
+	margin-bottom: 1em;
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.06);
+	margin-bottom: 3em;
+
+	header {
+		height: 80px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		h1 {
+			font-size: 14px;
+		}
+
+		a {
+			text-decoration: none;
+			color: var(--dark);
+		}
+
+		.dark-mode {
+			cursor: pointer;
+			font-size: 12px;
+			font-weight: 600;
+
+			i {
+				transform: rotate(-20deg);
+				margin-right: 10px;
+			}
+		}
+	}
+
+	@media screen and (min-width: 768px) {
+		header {
+			h1 {
+				font-size: 24px;
+			}
+
+			.dark-mode {
+				font-size: 1em;
+			}
+		}
+	}
+`;
+
+export default function Header({ setDarkMode, darkMode }) {
+	function handleDarkMode() {
+		setDarkMode(!darkMode);
+	}
+
+	return (
+		<HeaderStyled>
+			<Wrapper>
+				{darkMode}
+				<header>
+					<Link to='/'>
+						<h1>Where in the world?</h1>
+					</Link>
+					<div className='dark-mode' onClick={handleDarkMode}>
+						<span>
+							<i className='far fa-moon'></i> Dark Mode
+						</span>
+					</div>
+				</header>
+			</Wrapper>
+		</HeaderStyled>
+	);
+}
