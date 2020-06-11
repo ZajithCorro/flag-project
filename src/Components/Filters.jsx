656f4e1cda186e314491989c058ff712441af079
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-11 16:23:59 -0500

    Contenedor para multiples filtros

---
+++ src/Components/Filters.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Search from './Search';
+import Wrapper from './Wrapper';
+
+export default function Filters() {
+	return (
+		<Wrapper>
+			<Search />
+		</Wrapper>
+	);
+}
