4c2f3abcc0337335d7c72d9a15a91124cd7d9d88
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-11 16:28:56 -0500

    Agregando componente de filtros a la página principal

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import CountryList from '../Components/CountryList';
+import Filters from '../Components/Filters';
 
 export default function Home() {
-	return <CountryList />;
+	return (
+		<>
+			<Filters />
+			<CountryList />
+		</>
+	);
 }
