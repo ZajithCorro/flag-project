f30c94fb1b70272e975cac66f3aac6f93c49deb7
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-06-11 16:24:47 -0500

    Agregando funciones que permite realizar el filtrado por nombre de país. También permite recordar el valor del input del país ingresado

--- src/Context/CountryContext.jsx
+++ src/Context/CountryContext.jsx
@@ -5,16 +5,32 @@ const { Provider } = CountryContext;
 
 const initialState = {
 	countryList: [],
+	countryListByName: [],
+	inputSearch: '',
 };
 
 function CountryProvider({ children }) {
 	const [state, setState] = useState(initialState);
 
+	function setCountryByName(countryName) {
+		const { countryList } = state;
+
+		const countryListByName = countryList.filter((country) =>
+			country.name.toLowerCase().includes(countryName.toLowerCase())
+		);
+
+		setState({ ...state, countryListByName, inputSearch: countryName });
+	}
+
 	function setCountryList(countryList) {
 		setState({ ...state, countryList });
 	}
 
-	return <Provider value={{ state, setCountryList }}>{children}</Provider>;
+	return (
+		<Provider value={{ state, setCountryList, setCountryByName }}>
+			{children}
+		</Provider>
+	);
 }
 
 export { CountryContext, CountryProvider };
